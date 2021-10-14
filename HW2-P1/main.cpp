ba21bbc0b177918f81d62460753959c0d9b7f514
KimberlyModeste <KimberlyJModeste@gmail.com>
2021-10-13 22:23:46 -0400

    Dino Circles with colors finish

--- HW2-P1/main.cpp
+++ HW2-P1/main.cpp
@@ -8,8 +8,10 @@
 #include "classes.h"
 
 using namespace std;
+
 char title[] = "Dino Circle";
-Canvas cvs(640, 640, title);
+Canvas cvs(640, 480, title);
+
 
 void drawPolyLineFile(char* fileName)
 {
@@ -17,7 +19,7 @@ void drawPolyLineFile(char* fileName)
 	inStream.open(fileName, ios::in); // open the file
 	if (inStream.fail())
 		return;
-	glClear(GL_COLOR_BUFFER_BIT); // clear the screen
+	
 	GLint numpolys, numLines, x, y;
 	inStream >> numpolys; // read the number of polylines
 	for (int j = 0; j < numpolys; j++) // read each polyline
@@ -35,20 +37,83 @@ void drawPolyLineFile(char* fileName)
 	inStream.close();
 }
 
-void display() {
+void display2() {
+
+	char dino[] = "Brontoi.dat";
+	float theta;
+	float r = 1;
+	Point2 c;
 
 	cvs.clearScreen();
 	cvs.setBackgroundColor(1.0, 1.0, 1.0);
-	cvs.setColor(0.0, 0.0, 0.0);
-	cvs.setWindow(-10, 10, -10, 10);
-	cvs.setViewport(0, 640, 0, 640);
+	cvs.setViewport(0, 640, 0, 480);
+	cvs.setWindow(0, 640, 0, 480);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, 640, 0, 480);
+
+	glPushMatrix();
+	
+	//inner rotation
+	c.set(420, 120);
+	cvs.setColor(1.0, 0.0, 0.0);
+	glTranslatef(440, 30, 0.0);
+	glScalef(0.0625, 0.0625, 1.0);
+	for (int i = 0; i < 360; i += 20)
+	{
+		theta = i * 3.14159265 / 180;
+		glTranslatef(r * cos(theta) + c.getX(), r * sin(theta) + c.getY(), 0.0);
+		glRotatef(20.0f, 0.0, 0.0, 1.0);
+
+		drawPolyLineFile(dino);
+	}
+	glPopMatrix();
+
+	glPushMatrix();
+	//outer rotation
+	cvs.setColor(0.0, 1.0, 0.0);
+	glTranslatef(140, 420, 0.0);
+	glScalef(0.0625, 0.0625, 1.0);
+	for (int i = 0; i < 360; i += 10)
+	{
+		theta = i * 3.14159265 / 180;
+		glTranslatef(r * cos(theta) + c.getX(), r * sin(theta) + c.getY(), 0.0);
+		glRotatef(-20.0f, 0.0, 0.0, 1.0);
+
+		drawPolyLineFile(dino);
+	}
+	glPopMatrix();
+
+
+	//standing up
+	r = 90;
+	c.set(140, 120);
+	cvs.setColor(0.0, 0.0, 1.0);
+	for (int i = 0; i < 360; i += 20)
+	{
+		theta = i * 3.14159265 / 180;
+		glViewport(r * cos(theta) + c.getX(), r * sin(theta) + c.getY(), 40, 30);
+		drawPolyLineFile(dino);
+	}
+
+	//upside down
+	c.set(480, 340);
+	cvs.setColor(1.0, 0.0, 1.0);
+	cvs.setWindow(0, 640, 480, 0);
+	for (int i = 0; i < 360; i += 20)
+	{
+		theta = i * 3.14159265 / 180;
+		glViewport(r * cos(theta) + c.getX(), r * sin(theta) + c.getY(), 40, 30);
+		drawPolyLineFile(dino);
+	}
+
 
 }
 
-
 void main(void)
 {
-	glutDisplayFunc(display);
+	glutDisplayFunc(display2);
 	glutMainLoop();
 
 }
